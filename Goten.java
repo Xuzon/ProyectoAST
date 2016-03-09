c9736296609d72cc536554109ff2f39f5146b57a
Xuzon <xuzon69@gmail.com>
2016-03-09 16:48:41 +0100

    añadido goten

---
+++ Goten.java
@@ -0,0 +1,9 @@
+
+public interface Goten {
+	
+	public int comprobarApuestaPartido(int id_a);
+	public int comprobarApuestaPichichi(int id_a);
+	public int realizarApuestaPartido(int id_p, int goles_e1, int goles_e2);
+	public int realizarApuestaPichichi(int id_j);
+	
+}
