56f1a13e1d864719a1da4e3594872cfc687d06ee
brunonogareda <brunonogareda@gmail.com>
2016-04-13 18:35:11 +0200

    Added files via upload

---
+++ Goten/src/org/apache/ws/axis2/GohanMessageReceiverInOut.java
@@ -0,0 +1,230 @@
+/**
+ * GohanMessageReceiverInOut.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.0  Built on : Jan 18, 2016 (09:41:27 GMT)
+ */
+package org.apache.ws.axis2;
+
+
+/**
+ *  GohanMessageReceiverInOut message receiver
+ */
+public class GohanMessageReceiverInOut extends org.apache.axis2.receivers.AbstractInOutMessageReceiver {
+    public void invokeBusinessLogic(
+        org.apache.axis2.context.MessageContext msgContext,
+        org.apache.axis2.context.MessageContext newMsgContext)
+        throws org.apache.axis2.AxisFault {
+        try {
+            // get the implementation class for the Web Service
+            Object obj = getTheImplementationObject(msgContext);
+
+            GohanSkeleton skel = (GohanSkeleton) obj;
+
+            //Out Envelop
+            org.apache.axiom.soap.SOAPEnvelope envelope = null;
+
+            //Find the axisOperation that has been set by the Dispatch phase.
+            org.apache.axis2.description.AxisOperation op = msgContext.getOperationContext()
+                                                                      .getAxisOperation();
+
+            if (op == null) {
+                throw new org.apache.axis2.AxisFault(
+                    "Operation is not located, if this is doclit style the SOAP-ACTION should specified via the SOAP Action to use the RawXMLProvider");
+            }
+
+            java.lang.String methodName;
+
+            if ((op.getName() != null) &&
+                    ((methodName = org.apache.axis2.util.JavaUtils.xmlNameToJavaIdentifier(
+                            op.getName().getLocalPart())) != null)) {
+                if ("realizarPago".equals(methodName)) {
+                    org.apache.ws.axis2.RealizarPagoResponse realizarPagoResponse9 =
+                        null;
+                    org.apache.ws.axis2.RealizarPago wrappedParam = (org.apache.ws.axis2.RealizarPago) fromOM(msgContext.getEnvelope()
+                                                                                                                        .getBody()
+                                                                                                                        .getFirstElement(),
+                            org.apache.ws.axis2.RealizarPago.class);
+
+                    realizarPagoResponse9 = skel.realizarPago(wrappedParam);
+
+                    envelope = toEnvelope(getSOAPFactory(msgContext),
+                            realizarPagoResponse9, false,
+                            new javax.xml.namespace.QName(
+                                "http://ws.apache.org/axis2",
+                                "realizarPagoResponse"));
+                } else
+                 if ("abonarImporte".equals(methodName)) {
+                    org.apache.ws.axis2.AbonarImporteResponse abonarImporteResponse11 =
+                        null;
+                    org.apache.ws.axis2.AbonarImporte wrappedParam = (org.apache.ws.axis2.AbonarImporte) fromOM(msgContext.getEnvelope()
+                                                                                                                          .getBody()
+                                                                                                                          .getFirstElement(),
+                            org.apache.ws.axis2.AbonarImporte.class);
+
+                    abonarImporteResponse11 = skel.abonarImporte(wrappedParam);
+
+                    envelope = toEnvelope(getSOAPFactory(msgContext),
+                            abonarImporteResponse11, false,
+                            new javax.xml.namespace.QName(
+                                "http://ws.apache.org/axis2",
+                                "abonarImporteResponse"));
+                } else {
+                    throw new java.lang.RuntimeException("method not found");
+                }
+
+                newMsgContext.setEnvelope(envelope);
+            }
+        } catch (java.lang.Exception e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    //
+    @SuppressWarnings("unused")
+	private org.apache.axiom.om.OMElement toOM(
+        org.apache.ws.axis2.RealizarPago param, boolean optimizeContent)
+        throws org.apache.axis2.AxisFault {
+        try {
+            return param.getOMElement(org.apache.ws.axis2.RealizarPago.MY_QNAME,
+                org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+        } catch (org.apache.axis2.databinding.ADBException e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    @SuppressWarnings("unused")
+	private org.apache.axiom.om.OMElement toOM(
+        org.apache.ws.axis2.RealizarPagoResponse param, boolean optimizeContent)
+        throws org.apache.axis2.AxisFault {
+        try {
+            return param.getOMElement(org.apache.ws.axis2.RealizarPagoResponse.MY_QNAME,
+                org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+        } catch (org.apache.axis2.databinding.ADBException e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    @SuppressWarnings("unused")
+	private org.apache.axiom.om.OMElement toOM(
+        org.apache.ws.axis2.AbonarImporte param, boolean optimizeContent)
+        throws org.apache.axis2.AxisFault {
+        try {
+            return param.getOMElement(org.apache.ws.axis2.AbonarImporte.MY_QNAME,
+                org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+        } catch (org.apache.axis2.databinding.ADBException e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    @SuppressWarnings("unused")
+	private org.apache.axiom.om.OMElement toOM(
+        org.apache.ws.axis2.AbonarImporteResponse param, boolean optimizeContent)
+        throws org.apache.axis2.AxisFault {
+        try {
+            return param.getOMElement(org.apache.ws.axis2.AbonarImporteResponse.MY_QNAME,
+                org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+        } catch (org.apache.axis2.databinding.ADBException e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    private org.apache.axiom.soap.SOAPEnvelope toEnvelope(
+        org.apache.axiom.soap.SOAPFactory factory,
+        org.apache.ws.axis2.RealizarPagoResponse param,
+        boolean optimizeContent, javax.xml.namespace.QName elementQName)
+        throws org.apache.axis2.AxisFault {
+        try {
+            org.apache.axiom.soap.SOAPEnvelope emptyEnvelope = factory.getDefaultEnvelope();
+
+            emptyEnvelope.getBody()
+                         .addChild(param.getOMElement(
+                    org.apache.ws.axis2.RealizarPagoResponse.MY_QNAME, factory));
+
+            return emptyEnvelope;
+        } catch (org.apache.axis2.databinding.ADBException e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    @SuppressWarnings("unused")
+	private org.apache.ws.axis2.RealizarPagoResponse wraprealizarPago() {
+        org.apache.ws.axis2.RealizarPagoResponse wrappedElement = new org.apache.ws.axis2.RealizarPagoResponse();
+
+        return wrappedElement;
+    }
+
+    private org.apache.axiom.soap.SOAPEnvelope toEnvelope(
+        org.apache.axiom.soap.SOAPFactory factory,
+        org.apache.ws.axis2.AbonarImporteResponse param,
+        boolean optimizeContent, javax.xml.namespace.QName elementQName)
+        throws org.apache.axis2.AxisFault {
+        try {
+            org.apache.axiom.soap.SOAPEnvelope emptyEnvelope = factory.getDefaultEnvelope();
+
+            emptyEnvelope.getBody()
+                         .addChild(param.getOMElement(
+                    org.apache.ws.axis2.AbonarImporteResponse.MY_QNAME, factory));
+
+            return emptyEnvelope;
+        } catch (org.apache.axis2.databinding.ADBException e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    @SuppressWarnings("unused")
+	private org.apache.ws.axis2.AbonarImporteResponse wrapabonarImporte() {
+        org.apache.ws.axis2.AbonarImporteResponse wrappedElement = new org.apache.ws.axis2.AbonarImporteResponse();
+
+        return wrappedElement;
+    }
+
+    /**
+     *  get the default envelope
+     */
+    @SuppressWarnings("unused")
+	private org.apache.axiom.soap.SOAPEnvelope toEnvelope(
+        org.apache.axiom.soap.SOAPFactory factory) {
+        return factory.getDefaultEnvelope();
+    }
+
+    @SuppressWarnings("rawtypes")
+	private java.lang.Object fromOM(org.apache.axiom.om.OMElement param,
+        java.lang.Class type) throws org.apache.axis2.AxisFault {
+        try {
+            if (org.apache.ws.axis2.AbonarImporte.class.equals(type)) {
+                return org.apache.ws.axis2.AbonarImporte.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+            }
+
+            if (org.apache.ws.axis2.AbonarImporteResponse.class.equals(type)) {
+                return org.apache.ws.axis2.AbonarImporteResponse.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+            }
+
+            if (org.apache.ws.axis2.RealizarPago.class.equals(type)) {
+                return org.apache.ws.axis2.RealizarPago.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+            }
+
+            if (org.apache.ws.axis2.RealizarPagoResponse.class.equals(type)) {
+                return org.apache.ws.axis2.RealizarPagoResponse.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+            }
+        } catch (java.lang.Exception e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+
+        return null;
+    }
+
+    @SuppressWarnings("unused")
+	private org.apache.axis2.AxisFault createAxisFault(java.lang.Exception e) {
+        org.apache.axis2.AxisFault f;
+        Throwable cause = e.getCause();
+
+        if (cause != null) {
+            f = new org.apache.axis2.AxisFault(e.getMessage(), cause);
+        } else {
+            f = new org.apache.axis2.AxisFault(e.getMessage());
+        }
+
+        return f;
+    }
+} //end of class
