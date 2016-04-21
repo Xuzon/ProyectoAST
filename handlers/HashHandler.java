7cce3d7a3f0421c052be78dde1af6ac2408b11df
brunonogareda <brunonogareda@gmail.com>
2016-04-21 10:41:02 +0200

    Corrección del hash
    En la iteración donde se busca la cabecera, daba un error de casteo al pasar a soapheaderElement, se modifica para pasarlo a OMElement y trabajar directamente como si fuera un xml cualquiera. De esta forma buscamos el nombre de la cabecera que nos interesa y comprobamos el hash.

--- handlers/HashHandler.java
+++ handlers/HashHandler.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 
 import javax.xml.soap.SOAPHeaderElement;
 
+import org.apache.axiom.om.OMElement;
 import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axiom.soap.SOAPHeader;
 import org.apache.axis2.AxisFault;
@@ -15,6 +16,13 @@ public class HashHandler extends AbstractHandler{
 	private String hash = "";
 	private String funcionAAplicar = "";
 	private static boolean enabled = true;
+	
+	public void main() throws AxisFault
+	 {
+		MessageContext x = new MessageContext();
+		invoke(x);
+	 }
+	
 	@Override
 	public InvocationResponse invoke(MessageContext arg0) throws AxisFault {
 		if(!enabled)//si lo desactivo por parametros siempre voy a devolver continuar
@@ -25,7 +33,7 @@ public class HashHandler extends AbstractHandler{
 			return InvocationResponse.CONTINUE;
 		}
 		boolean pasar = comprobarCabecera(arg0);//compruebo la cabecera
-		if(pasar)//y según la comprobacion devuelvo un continue o un aborto
+		if(pasar)//y segun la comprobacion devuelvo un continue o un aborto
 			return InvocationResponse.CONTINUE;
 		else
 			return InvocationResponse.ABORT;
@@ -39,7 +47,7 @@ public class HashHandler extends AbstractHandler{
 			SOAPEnvelope se = msg.getEnvelope();
 			SOAPHeader sh = se.getHeader();
 			Iterator it = sh.examineAllHeaderBlocks();
-			SOAPHeaderElement hel;
+			OMElement hel;
 			boolean aplicar = false;//no se aplica para esta funcion
 			if(funcionAAplicar.equals("ALL"))//si directamente es a todas las funciones
 				aplicar = true;//aplico a true
@@ -48,10 +56,10 @@ public class HashHandler extends AbstractHandler{
 			if(!aplicar)//si no se aplica el hash porque no es esa funcion
 				return true;//devuelve true
 			while (it.hasNext()) {
-				hel = (SOAPHeaderElement) it.next();
-			    String headerName = hel.getNodeName();
+				hel = (OMElement) it.next();
+				String headerName = hel.getLocalName();
 			    if(headerName.equals(cabeceraHash)){//si la cabecera es la cabeceraHash
-				    if(hel.getTextContent().equals(hash))//consigo el valor del hash y si vale el valor
+				    if(hel.getText().equals(hash))//consigo el valor del hash y si vale el valor
 				    	return true;//devuelvo true
 			    }
 			}
